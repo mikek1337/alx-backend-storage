e09e172af0d3c890c7439cb3e60222eab5f81361
mikek1337 <mikekegnlyew@gmail.com>
2023-07-12 19:03:59 +0300

    create user table with country enum

---
+++ 0x00-MYSQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create user table
+
+CREATE TABLE IF NOT EXISTS users(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE KEY,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+)
