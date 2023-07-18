7275163198e8370937c8680aec820ee15275ae5d
mikek1337 <mikekegnlyew@gmail.com>
2023-07-18 20:41:35 +0300

    display logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient()
+    collection = client.logs.nginx
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    print("\tmethod GET: {}".format(
+        collection.count_documents({"method": "GET"})))
+    print("\tmethod POST: {}".format(
+        collection.count_documents({"method": "POST"})))
+    print("\tmethod PUT: {}".format(
+        collection.count_documents({"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(
+        collection.count_documents({"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(
+        collection.count_documents({"method": "DELETE"})))
+    print("{} status check".format(
+        collection.count_documents({"method": "GET", "path": "/status"})))
