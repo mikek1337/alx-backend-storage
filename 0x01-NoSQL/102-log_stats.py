487b69106c0d7317031ad1699140d153da60e825
mikek1337 <mikekegnlyew@gmail.com>
2023-07-18 21:07:22 +0300

    update log_stat to show top 10 ip

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" provides some stats about Nginx logs stored in MongoDB """
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient()
+    collection = client.logs.nginx
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    print("\tmethod GET: {}".format(
+        collection.count_documents({"method": "GET"})))
+    print("\tmethod POST: {}".format(
+        collection.count_documents({"method": "POST"})))
+    print("\tmethod PUT: {}".format(
+        collection.count_documents({"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(
+        collection.count_documents({"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(
+        collection.count_documents({"method": "DELETE"})))
+    print("{} status check".format(
+        collection.count_documents({"method": "GET", "path": "/status"})))
+    print("IPs:")
+    ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in ips:
+        print("\t{}: {}".format(ip.get("_id"), ip.get("count")))
