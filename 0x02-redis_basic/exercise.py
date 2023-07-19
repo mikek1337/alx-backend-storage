380fa6f300273eb9d0ad1a2f2fa77499372c70a1
mikek1337 <mikekegnlyew@gmail.com>
2023-07-19 22:29:51 +0300

    updated style and added get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,37 +5,42 @@ from typing import Union, Callable
 import uuid
 
 
+def count_calls(method: Callable) -> Callable:
+    """count how many times methods of the Cache class are called"""
+    key = method.__qualname__
+
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
 class Cache:
     """class to handle caching by redis"""
 
     def __init__(self) -> None:
         """constructor"""
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, int, float, bytes]) -> str:
         """store data in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get_str(self, key: str) -> str:
+    def get(self, key: str, fn: Callable) -> Union[str, int, float, bytes]:
         """get data from redis"""
         data = self._redis.get(key)
-        if data is not None:
-            return data.decode('utf-8')
-        return ""
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """get data from redis"""
+        return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
         """get data from redis"""
-        data = self._redis.get(key)
-        if data is not None:
-            return int(data.decode('utf-8'))
-        return 0
+       return self.get(key, lambda d: int(d))
 
-    def get(self, key: str, fn: Callable) -> str:
-        """get data from redis"""
-        data = self._redis.get(key)
-        if data is not None:
-            return fn(data)
-        return ""
